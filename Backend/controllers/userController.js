308086c300ddf5cec5979ae8f296cc326627f5f8
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-08-17 15:34:49 +0500

    authentication and authorization done

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -13,9 +13,35 @@ exports.registerUser = catchAsyncErrors(async(req,res,next)=>{
              url:"profilepicUrl"   
         }
     })
+    const token = user.getJWTToken();
     res.status(201).json({
         success:true,
+        token, 
         user,
-
     })
+})
+
+//Login a user
+exports.loginUser = catchAsyncErrors(async(req,res,next)=>{
+    const {email,password}=req.body;
+    //checking if user has given 
+    if(!email || !password){
+      return next(new ErrorHandler("Please Enter Email & Password",400))
+    }
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+        return next(new ErrorHandler("Invalid email or password",401))
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid email or password",401))
+    }  
+    else{
+        const token = user.getJWTToken();
+        res.status(200).json({
+            success:true,
+            token, 
+        })
+    }
+    
 })
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser} = require('../controllers/userController')
-router.route('./register').post((registerUser));
+const {registerUser,loginUser} = require('../controllers/userController')
+router.route('/register').post((registerUser));
+router.route('/login').post((loginUser))
 module.exports = router;
