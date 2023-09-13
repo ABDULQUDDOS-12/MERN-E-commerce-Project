fe470adb58560aefaa1adb463342cd3c1eda0273
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-09-13 17:08:16 +0500

    User Routes completed

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -7,8 +7,24 @@ module.exports = (err, req, res, next) => {
     const message = `Resource not found. Invalid: ${err.path}`;
     err = new ErrorHandler(message,400);
   }
+//mongoose duplicate key error
+  if(err.code===11000){
+  const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+  err = new ErrorHandler(message,400)
+}
+//wrong jwt token
+if(err.name === "JsonwebTokenError" ){
+  const message = `Json Web Token is invalid, try again`;
+  err = new ErrorHandler(message,400);
+}
+//JWT Expire error
+if(err.name === "TokenExpiredError" ){
+  const message = `Json Web Token is Expired, try again`;
+  err = new ErrorHandler(message,400);
+}
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
   });
+
 };
--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -5,4 +5,5 @@ const router = express.Router();
 router.route('/products').get( getAllProducts)
 router.route('/products/new').post(isAuthenticatedUser,authorizeRoles('admin'),createproduct)
 router.route('/products/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateProducts).get(getProductDetails).delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProducts)
+router.route("/product/:id").get(getProductDetails)    
 module.exports = router
