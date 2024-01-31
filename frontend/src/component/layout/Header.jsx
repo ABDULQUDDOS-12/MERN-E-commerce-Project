293471145d4f25494db808f0dd6da591d42cd472
Abdul quddos <abdulquddoshafiz@gmail.com>
2024-01-31 17:20:38 +0500

    Frontend (Navbar Started)

---
+++ frontend/src/component/layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {ReactNavbar} from 'overlay-navbar'
+const Header = () => {
+  return ( 
+     <ReactNavbar
+     burgerColor = '#eb4034'
+     burgerColorHover='#a62d24'
+     navColor1 = "white"
+     logoHoverSize="10px"
+     logoHoverColor="#eb4034"
+     link1Text="Home"
+     />
+    )
+}
+
+export default Header
