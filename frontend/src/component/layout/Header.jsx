853e8408d6feca444727a383beebd6b340b59086
Abdul quddos <abdulquddoshafiz@gmail.com>
2024-02-21 23:20:08 +0500

    links created

--- frontend/src/component/layout/Header.jsx
+++ frontend/src/component/layout/Header.jsx
@@ -9,6 +9,13 @@ const Header = () => {
      logoHoverSize="10px"
      logoHoverColor="#eb4034"
      link1Text="Home"
+     link2Text="Product"
+     link3Text="Contact"
+     link4Text="About"
+     link1Url="/"
+     link2Url="/"
+     link3Url="/"
+     link4Url="/"
      />
     )
 }
