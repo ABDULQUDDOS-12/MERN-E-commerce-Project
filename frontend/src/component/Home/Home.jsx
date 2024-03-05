82b3ef1b931a64e0b242262a68636aabd831dc71
Abdul quddos <abdulquddoshafiz@gmail.com>
2024-03-05 22:36:11 +0500

    Fething products on the front end

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -4,21 +4,22 @@ import './Home.css'
 import Product from './Product.jsx'
 import MetaData from '../layout/MetaData.jsx'
 import { getProduct } from '../../Actions/productAction.jsx'
-import {useDispatch} from "react-redux"
-const product = {
-  name:"Blue Tshirt",
-  images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
-  price:"RS3000",
-  _id:"Abdul Quddos"
-}
+import {useSelector,useDispatch} from "react-redux"
+// const product = {
+//   name:"Blue Tshirt",
+//   images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
+//   price:"RS3000",
+//   _id:"Abdul Quddos"
+// }
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProduct());
   }, [dispatch])
-  
+  const{loading,error,products,productsCount} = useSelector(state=>state.products)
 return (
-    <Fragment>
+<Fragment>
+  {loading ? "Loading" :      <Fragment>
         <MetaData title={"Ecommerce"}></MetaData>
         <div className="banner">
             <p>Welcome to ecommerce</p>
@@ -31,16 +32,10 @@ return (
         </div>
         <h2 className="homeHeading">Featured Product</h2>
         <div className="container" id="container">
-        <Product product={product}></Product>
-        <Product product={product}></Product>
-        <Product product={product}></Product>
-        <Product product={product}></Product>
-        <Product product={product}></Product>
-        <Product product={product}></Product>
-        <Product product={product}></Product>
-        <Product product={product}></Product>
+        {products && products.map((product)=><Product product={product}/>)}
         </div>
-    </Fragment>
+    </Fragment>}
+</Fragment>
   )
 }
 
